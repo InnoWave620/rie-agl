57f60bd732492f7fc5f24abdbe04a25fc901363f
Senzo Shinga <innowave620@gmail.com>
2026-06-07 19:06:12 +0200

    fix: suppress hydration warning on root html and body tags to ignore browser extension attribute injections

--- rie.agl/app/layout.tsx
+++ rie.agl/app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${inter.variable} h-full`}>
-      <body className="min-h-full flex flex-col antialiased">{children}</body>
+    <html lang="en" className={`${inter.variable} h-full`} suppressHydrationWarning>
+      <body className="min-h-full flex flex-col antialiased" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
